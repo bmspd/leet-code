5b3d277e7d29102bd7444ba456299bb53467d39a
Leonor Myung <lmyung@pr-h3.kzn.21-school.ru>
2022-08-27 23:45:54 +0300

    ex2 - not finished

---
+++ medium/2.Add_two_numbers.ts
@@ -0,0 +1,65 @@
+class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.next = (next===undefined ? null : next)
+      }
+}
+
+function printListNode(list: ListNode): void {
+  let ptr = list;
+  let res = ''
+  while(ptr) {
+    res += `${ptr.val}${ptr.next ? '->' : '.'}`
+    ptr = ptr.next
+  }
+  console.log(res)
+}
+
+function getNumberFromList(list: ListNode):number {
+  let ptr = list
+  let number = ''
+  while (ptr) {
+    number += String(ptr.val)
+    ptr = ptr.next
+  }
+  return Number(number)
+}
+
+function makeListFromNumber(number: string): ListNode | null {
+  if (number.length === 1) return new ListNode(Number(number))
+  return new ListNode(Number(number[0]), makeListFromNumber(number.slice(1)))
+}
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const first = Number(String(getNumberFromList(l1)).split('').reverse().join(''))
+  const second = Number(String(getNumberFromList(l2)).split('').reverse().join(''))
+  const reversedSum = String(first + second).split('').reverse().join('')
+  return makeListFromNumber(reversedSum)
+}
+/*
+// 243 + 564 => 807 ---- 708
+let first = new ListNode(2, new ListNode(4, new ListNode(3)))
+let second = new ListNode(5, new ListNode(6, new ListNode(4)))
+printListNode(first)
+printListNode(second)
+printListNode(addTwoNumbers(first, second))
+// 0 + 0 => 0 ---- 0
+first = new ListNode(0)
+second = new ListNode(0)
+printListNode(first)
+printListNode(second)
+printListNode(addTwoNumbers(first, second))
+// 9999999 + 9999 => 10009998 ---- 89990001
+first = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))))
+second = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+printListNode(first)
+printListNode(second)
+printListNode(addTwoNumbers(first, second))*/
+// 243 + 564 => 807 ---- 708
+let first = new ListNode(2, new ListNode(4, new ListNode(9)))
+let second = new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(9))))
+printListNode(first)
+printListNode(second)
+printListNode(addTwoNumbers(first, second))
